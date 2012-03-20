7db5b30ba801a2c6d6ea1682f01a20ddcb511b6c
moreginger <moreginger@warpmail.net>
2012-03-20 22:22:53 +0000

    Method to parse subtitles json and return a list of dicts.
    https://github.com/moreginger/xbmc-plugin.video.ted.talks/issues/10

--- resources/lib/model/subtitles_scraper.py
+++ resources/lib/model/subtitles_scraper.py
@@ -14,9 +14,24 @@ def format_time(time):
     seconds = (time / 1000) % 60
     minutes = (time / 60000) % 60
     hours = (time / 3600000)
-    return "%02d:%02d:%02d,%03d" % (hours, minutes, seconds, millis)
+    return '%02d:%02d:%02d,%03d' % (hours, minutes, seconds, millis)
 
 def get_languages(soup):
     languages = soup.find('select', attrs = {'id': 'languageCode'}).findAll('option')
     return [l['value'].encode('ascii') for l in languages]
 
+def get_subtitles(talk_id, language):
+    url = 'http://www.ted.com/talks/subtitles/id/%s/lang/%s' % (talk_id, language)
+    return get_subtitles_for_url(url)
+        
+def get_subtitles_for_url(url):
+    s = urllib.urlopen(url)
+    try:
+        json = simplejson.load(s)
+    finally:
+        s.close()
+
+    captions = []
+    for caption in json['captions']:
+        captions += [{'start': caption['startTime'], 'duration': caption['duration'], 'content': caption['content']}]
+    return captions
--- resources/lib/model/subtitles_scraper_test.py
+++ resources/lib/model/subtitles_scraper_test.py
@@ -1,7 +1,14 @@
 import unittest
 import subtitles_scraper
 import urllib
+import tempfile
 from BeautifulSoup import MinimalSoup
+
+def get_talk_1253():
+    '''
+    Return soup for an arbitrary fixed talk.
+    '''
+    return MinimalSoup(urllib.urlopen('http://www.ted.com/talks/richard_wilkinson.html').read())
 
 class TestSubtitlesScraper(unittest.TestCase):
     
@@ -10,6 +17,22 @@ class TestSubtitlesScraper(unittest.TestCase):
         self.assertEqual('03:25:45,678', subtitles_scraper.format_time(12345678))
 
     def test_get_languages(self):
-        soup = MinimalSoup(urllib.urlopen('http://www.ted.com/talks/richard_wilkinson.html').read())
+        soup = get_talk_1253()
         expected = ['sq', 'ar', 'hy', 'bg', 'ca', 'zh-cn', 'zh-tw', 'hr', 'cs', 'da', 'nl', 'en', 'fr', 'ka', 'de', 'el', 'he', 'hu', 'id', 'it', 'ja', 'ko', 'fa', 'pl', 'pt-br', 'pt', 'ro', 'ru', 'sr', 'sk', 'es', 'th', 'tr', 'uk', 'vi']
         self.assertEqual(expected, subtitles_scraper.get_languages(soup))
+        
+    def test_get_subtitles(self):
+        subs = subtitles_scraper.get_subtitles('1253', 'en')
+        self.assertEqual(385, len(subs))
+    
+    def test_get_subtitles_for_url(self):
+        json_subs = '{"captions":[{"content":"What","startTime":0,"duration":3000,"startOfParagraph":false},{"content":"Began","startTime":3000,"duration":4000,"startOfParagraph":false}]}'
+        subs_file = tempfile.NamedTemporaryFile()
+        try:  
+            subs_file.write(json_subs)
+            subs_file.flush()
+            subs = subtitles_scraper.get_subtitles_for_url(subs_file.name)
+        finally:
+            subs_file.close()
+        self.assertEqual([{'duration': 3000, 'start': 0, 'content': 'What'}, {'duration': 4000, 'start': 3000, 'content': 'Began'}], subs)
+    

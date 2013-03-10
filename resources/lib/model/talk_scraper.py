af38baff0979a47a267ecc5e23ea1ea3ca1d3903
Tim Moore <moreginger@warpmail.net>
2013-03-10 16:22:22 +0000

    Reimplement new talk scraper using parseDOM. It's much faster (1.3s -> 0.05s) but currently broken for YouTube links.

--- resources/lib/model/talk_scraper.py
+++ resources/lib/model/talk_scraper.py
@@ -1,5 +1,7 @@
-from BeautifulSoup import MinimalSoup
+
 import re
+# Custom xbmc thing for fast parsing. Can't rely on lxml being available as of 2012-03.
+import CommonFunctions.parseDOM as parseDOM
 
 class Talk:
 
@@ -8,31 +10,16 @@ class Talk:
 
     def get(self, url):
         html = self.get_HTML(url)
-        url = ""
-        soup = MinimalSoup(html)
-        # get title
-        title = soup.find('span', attrs={'id':'altHeadline'}).string
-        # get speaker from title
-        speaker = title.split(':', 1)[0]
-        # get description:
-        plot = soup.find('p', attrs={'id':'tagline'}).string
-        # get url
-        # detectors for link to video in order of preference
-        linkDetectors = [
-            lambda l: re.compile('High-res video \(MP4\)').match(str(l.string)),
-            lambda l: re.compile('http://download.ted.com/talks/.+.mp4').match(str(l['href'])),
-        ]
-        for link in soup.findAll('a', href=True):
-            for detector in linkDetectors:
-                if detector(link):
-                    url = link['href']
-                    linkDetectors = linkDetectors[:linkDetectors.index(detector)]  # Only look for better matches than what we have
-                    break
+        headline = parseDOM(html, 'span', attrs={'id':'altHeadline'})[0].split(':', 1)
+        speaker = headline[0].strip()
+        title = headline[1].strip()
+        plot = parseDOM(html, 'p', attrs={'id':'tagline'})[0]
 
-        if url == "":
-            # look for utub link
-            utublinks = re.compile('http://(?:www.)?youtube.com/v/([^\&]*)\&').findall(html)
-            for link in utublinks:
-                url = 'plugin://plugin.video.youtube/?action=play_video&videoid=%s' % (link)
+        url = ""
+        link_re = re.compile('http://download.ted.com/talks/.+.mp4')
+        for link in parseDOM(html, 'a', ret='href'):
+            if link_re.match(link):
+                url = link
+                break
 
         return url, title, speaker, plot
--- resources/lib/model/talk_scraper_test.py
+++ resources/lib/model/talk_scraper_test.py
@@ -14,20 +14,31 @@ def timing_run(html):
 
 class TestTalkScraper(unittest.TestCase):
 
-    def setUp(self):
+    def test_get_ted_video(self):
         self.scraper = Talk(get_HTML)
+        self.assert_talk_details("http://www.ted.com/talks/ariel_garten_know_thyself_with_a_brain_scanner.html", "http://download.ted.com/talks/ArielGarten_2011X.mp4", u"Know thyself, with a brain scanner", u"Ariel Garten")
 
-    def test_smoke(self):
+    # TODO How do these work? Are YouTube videos automagically played?
+    def test_get_youtube_video(self):
+        self.scraper = Talk(get_HTML)
+        self.assert_talk_details("http://www.ted.com/talks/bjarke_ingels_hedonistic_sustainability.html", "http://youtubesomething", u"Hedonistic Sustainability", u"Bjarke Ingels")
+
+    def assert_talk_details(self, talk_url, expected_video_url, expected_title, expected_speaker):
+        video_url, title, speaker, plot = self.scraper.get(talk_url)
+        self.assertEqual(expected_video_url, video_url)
+        self.assertEqual(expected_title, title)
+        self.assertEqual(expected_speaker, speaker)
+        self.assertTrue(plot)  # Not null or empty.
+
+    def test_performance(self):
         html = get_HTML("http://www.ted.com/talks/ariel_garten_know_thyself_with_a_brain_scanner.html")
 
         def test():
             timing_run(html);
 
         t = timeit.Timer(test)
-        self.assertGreater(10, t.timeit(10))
-
-#        self.talk.get("http://www.ted.com/talks/ariel_garten_know_thyself_with_a_brain_scanner.html")
-#        self.ted_talks.getVideoDetails("http://www.ted.com/talks/bjarke_ingels_hedonistic_sustainability.html")
-
-
+        repeats = 10
+        time = t.timeit(repeats)
+        print "Extracting speaker took %s seconds per run" % (time / repeats)
+        self.assertGreater(4, time)
 

a3ac92a32c2ce8a3e473e013c1276d934f27d054
Tim Moore <moreginger@warpmail.net>
2013-03-14 23:14:29 +0000

    Allow a custom video quality to be specified (at the model level, user can't do it yet).
    https://github.com/moreginger/xbmc-plugin.video.ted.talks/issues/37

--- resources/lib/model/talk_scraper.py
+++ resources/lib/model/talk_scraper.py
@@ -2,9 +2,15 @@
 import re
 # Custom xbmc thing for fast parsing. Can't rely on lxml being available as of 2012-03.
 import CommonFunctions as xbmc_common
+import httplib
+import urlparse
 
-def get(html):
-    """Extract talk details from talk html"""
+__download_link_re = re.compile('http://download.ted.com/talks/.+.mp4')
+
+def get(html, video_quality=3):
+    """Extract talk details from talk html
+       @param video_quality 1-7 with 7 being highest. 3 = standard.
+    """
 
     headline = xbmc_common.parseDOM(html, 'span', attrs={'id':'altHeadline'})[0].split(':', 1)
     speaker = headline[0].strip()
@@ -12,12 +18,25 @@ def get(html):
     plot = xbmc_common.parseDOM(html, 'p', attrs={'id':'tagline'})[0]
 
     url = None
-    if not url:
-        link_re = re.compile('http://download.ted.com/talks/.+.mp4')
-        for link in xbmc_common.parseDOM(html, 'a', ret='href'):
-            if link_re.match(link):
-                url = link
-                break
+    for link in xbmc_common.parseDOM(html, 'a', ret='href'):
+        if __download_link_re.match(link):
+            url = link
+            break
+
+    # We could confirm these URLs exist from the DOM (with difficulty) but seems likely to break
+    if url and not video_quality == 3:
+        # Quality '42' for testing
+        video_quality_string = {1:'64', 2:'180', 4:'450', 5:'600', 6:'950', 7:'1500', 42:'42'}[video_quality]
+        url_custom = url.replace(".mp4", "-%sk.mp4" % (video_quality_string))
+
+        # Test URL exists
+        url_custom_parsed = urlparse.urlparse(url_custom)
+        h = httplib.HTTPConnection(url_custom_parsed.netloc)
+        h.request('HEAD', url_custom_parsed.path)
+        response = h.getresponse()
+        h.close()
+        if response.status / 100 < 4:
+            url = url_custom
 
     if not url:
         youtube_re = re.compile('https?://.*?youtube.com/.*?/([^/?]+)')
--- resources/lib/model/talk_scraper_test.py
+++ resources/lib/model/talk_scraper_test.py
@@ -7,9 +7,6 @@ import timeit
 def get_HTML(url):
     return urllib2.urlopen(url).read()
 
-def timing_run(html):
-    talk_scraper.get(html);
-
 
 class TestTalkScraper(unittest.TestCase):
 
@@ -29,11 +26,37 @@ class TestTalkScraper(unittest.TestCase):
         self.assertEqual(expected_speaker, speaker)
         self.assertTrue(plot)  # Not null or empty.
 
+    def test_get_custom_quality_video(self):
+        html = get_HTML("http://www.ted.com/talks/edith_widder_how_we_found_the_giant_squid.html")
+        # Note not customized. Should be a useful fallback if this code goes haywire.
+        self.assert_custom_quality_url(html, 3, "http://download.ted.com/talks/EdithWidder_2013.mp4")
+
+        self.assert_custom_quality_url(html, 1, "http://download.ted.com/talks/EdithWidder_2013-64k.mp4")
+        self.assert_custom_quality_url(html, 2, "http://download.ted.com/talks/EdithWidder_2013-180k.mp4")
+        self.assert_custom_quality_url(html, 4, "http://download.ted.com/talks/EdithWidder_2013-450k.mp4")
+        self.assert_custom_quality_url(html, 5, "http://download.ted.com/talks/EdithWidder_2013-600k.mp4")
+        self.assert_custom_quality_url(html, 6, "http://download.ted.com/talks/EdithWidder_2013-950k.mp4")
+        self.assert_custom_quality_url(html, 7, "http://download.ted.com/talks/EdithWidder_2013-1500k.mp4")
+
+        # Fall back to standard URL when custom URL 404s
+        self.assert_custom_quality_url(html, 42, "http://download.ted.com/talks/EdithWidder_2013.mp4")
+
+        try:
+            self.assert_custom_quality_url(html, 8, "will blow up")
+            self.fail()
+        except KeyError:
+            pass
+
+
+    def assert_custom_quality_url(self, talk_html, video_quality, expected_video_url):
+        video_url, title, speaker, plot = talk_scraper.get(talk_html, video_quality)
+        self.assertEqual(expected_video_url, video_url)
+
     def test_performance(self):
         html = get_HTML("http://www.ted.com/talks/ariel_garten_know_thyself_with_a_brain_scanner.html")
 
         def test():
-            timing_run(html);
+            talk_scraper.get(html);
 
         t = timeit.Timer(test)
         repeats = 10

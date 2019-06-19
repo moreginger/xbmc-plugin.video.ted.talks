1b748e082a60f15dfa4d61fc1ad15a019a04905b
Tim Moore <moreginger@warpmail.net>
2019-06-19 21:49:52 +0100

    fix: Improve build script

--- build.sh
+++ build.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-BUILD=build
+BUILD=plugin.video.ted.talks
 DIST=dist.zip
 
 rm -r $BUILD || true
 mkdir $BUILD
-find -type f | grep -v "$BUILD\|$DIST\|build\.sh\|_test\.py\|test_utils\.py\|run_tests\.sh\|\.gitignore\|\.pyc\|\.travis.yml\|\.md\|README\|\.git\|\.pylintrc\|\testSupport\|requirements\.txt" | while read f; do cp $f $BUILD; done
+# find -type f | grep -v "$BUILD\|$DIST\|build\.sh\|_test\.py\|test_utils\.py\|run_tests\.sh\|\.gitignore\|\.pyc\|\.travis.yml\|\.md\|README\|\.git\|\.pylintrc\|\testSupport\|requirements\.txt" | while read f; do cp "$f" "$BUILD/$f"; done
+rsync -aP\
+  --exclude "$DIST"\
+  --exclude "$BUILD"\
+  --exclude '.git'\
+  --exclude '.gitignore'\
+  --exclude '.pylintrc'\
+  --exclude '.travis.yml'\
+  --exclude '*.sh'\
+  --exclude '*.md'\
+  --exclude 'requirements.txt'\
+  --exclude 'README'\
+  --exclude 'testSupport'\
+  --exclude '*_test.py'\
+  --exclude 'test_util.py'\
+  --exclude '*.pyc'\
+  ./ $BUILD/
 
 rm $DIST || true
 zip -r $DIST $BUILD

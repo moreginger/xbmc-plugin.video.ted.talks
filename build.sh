73a148e7b039bce0cc6422033511610d51fd84f4
Tim Moore <moreginger@warpmail.net>
2019-06-19 21:34:03 +0100

    fix: Build installable zip

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+BUILD=build
 DIST=dist.zip
+
+rm -r $BUILD || true
+mkdir $BUILD
+find -type f | grep -v "$BUILD\|$DIST\|build\.sh\|_test\.py\|test_utils\.py\|run_tests\.sh\|\.gitignore\|\.pyc\|\.travis.yml\|\.md\|README\|\.git\|\.pylintrc\|\testSupport\|requirements\.txt" | while read f; do cp $f $BUILD; done
+
 rm $DIST || true
-find -type f | grep -v 'build\.sh\|_test\.py\|test_utils\.py\|run_tests\.sh\|\.gitignore\|\.pyc\|\.travis.yml\|\.md\|README\|\.git\|\.pylintrc\|\testSupport\|requirements\.txt' | zip -@ $DIST 
+zip -r $DIST $BUILD

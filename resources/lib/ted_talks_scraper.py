4042f41226283877e44702c7cbafbd232865e3f8
Tim Moore <moreginger@warpmail.net>
2021-04-28 09:53:49 +0100

    Load subtitles with fetcher.

--- resources/lib/ted_talks_scraper.py
+++ resources/lib/ted_talks_scraper.py
@@ -26,19 +26,21 @@ class TedTalks:
         return playlist, title, subs, {'Director':speaker, 'Genre':'TED', 'Plot':plot, 'PlotOutline':plot}
 
     def __process_m3u8__(self, m3u8_url):
-        playlist = m3u8.load(m3u8_url)
+        r = self.fetcher.get(m3u8_url)
+        playlist = m3u8.loads(r.text)
+        base_url = r.url.rsplit('/', 1)[0] + '/'
 
         for x in [x for x in playlist.media]:
             if x.type == 'SUBTITLES':
                 playlist.media.remove(x)
             else:
-                x.uri = playlist.base_uri + x.uri
+                x.uri = base_url+ x.uri
         for x in playlist.playlists:
-            x.uri = playlist.base_uri + x.uri
+            x.uri = base_url+ x.uri
             for y in [y for y in x.media]:
                 if y.type == 'SUBTITLES':
                     x.media.remove(y)
         for x in playlist.iframe_playlists:
-            x.uri = playlist.base_uri + x.uri
+            x.uri = base_url+ x.uri
         
         return playlist.dumps()
